480915bf17c7e2f8997861fa48e9d8c24b46acc0
Dharmaveera18 <h.dharmaveera1998@gmail.com>
2024-08-20 10:02:34 +0530

    Practice commit to src main

---
+++ Selenium/src/main/java/com/methods/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.methods;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("If not u who? , This challenge before u because u can do it for sure.");
+
+	}
+
+}
